b540ebdc61b8847717dd5b4323de6c8975649212
Limssly <seydinalimamoulayeyade@gmail.com>
2024-05-07 16:25:42 +0000

    Modif page inscription

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,23 +2,37 @@
     <form method="POST" action="{{ route('register') }}">
         @csrf
 
-        <!-- Name -->
+        <!-- Prenom -->
         <div>
-            <x-input-label for="name" :value="__('Name')" />
+            <x-input-label for="firstname" :value="__('Votre prénom')" />
+            <x-text-input id="firstname" class="block mt-1 w-full" type="text" name="firstname" :value="old('firstname')" required autofocus autocomplete="firstname" />
+            <x-input-error :messages="$errors->get('firstname')" class="mt-2" />
+        </div>
+
+        <!-- Nom -->
+        <div>
+            <x-input-label for="name" :value="__('Votre nom')" />
             <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
             <x-input-error :messages="$errors->get('name')" class="mt-2" />
         </div>
 
-        <!-- Email Address -->
-        <div class="mt-4">
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+        <!-- Date de naissance -->
+        <div>
+            <x-input-label for="ddn" :value="__('Votre date de naissance')" />
+            <x-text-input id="ddn" class="block mt-1 w-full" type="date" name="ddn" :value="old('ddn')" required autofocus autocomplete="ddn" />
+            <x-input-error :messages="$errors->get('ddn')" class="mt-2" />
         </div>
 
-        <!-- Password -->
+        <!-- Addresse -->
+        <div>
+            <x-input-label for="adresse" :value="__('Adresse domicile')" />
+            <x-text-input id="adresse" class="block mt-1 w-full" type="text" name="adresse" :value="old('adresse')" required autocomplete="adresse" />
+            <x-input-error :messages="$errors->get('adresse')" class="mt-2" />
+        </div>
+
+        <!-- Mot de passe -->
         <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
+            <x-input-label for="password" :value="__('Mot de passe*')" />
 
             <x-text-input id="password" class="block mt-1 w-full"
                             type="password"
@@ -28,9 +42,9 @@
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
 
-        <!-- Confirm Password -->
+        <!-- Confirmation mot de passe -->
         <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
+            <x-input-label for="password_confirmation" :value="__('Confirmation mot de passe*')" />
 
             <x-text-input id="password_confirmation" class="block mt-1 w-full"
                             type="password"
@@ -41,11 +55,11 @@
 
         <div class="flex items-center justify-end mt-4">
             <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
-                {{ __('Already registered?') }}
+                {{ __('Vous avez déjà un compte?') }}
             </a>
 
             <x-primary-button class="ms-4">
-                {{ __('Register') }}
+                {{ __('Se connecter') }}
             </x-primary-button>
         </div>
     </form>
