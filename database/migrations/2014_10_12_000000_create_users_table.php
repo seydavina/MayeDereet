d0336231625949ec144a34bff6a5382fd9dc87ab
Limssly <seydinalimamoulayeyade@gmail.com>
2024-05-07 15:15:42 +0000

    User Table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,6 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('grade')->nullable();
-            $table->string('grade')->nullable();
             $table->date('date_of_birth')->nullable();
             $table->string('blood_group')->nullable();
             $table->integer('weight')->nullable();
